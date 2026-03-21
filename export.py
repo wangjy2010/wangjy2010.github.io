410096c17cf7a0e1c01548c6b7567606bec358a5
Wang Jingyang <jywang602@gmail.com>
2026-03-21 12:08:35 +0800

    remove export.py cache

--- export.py
+++
@@ -1,4 +0,0 @@
-with open('bayeselo_games.txt', 'r', encoding='utf-8') as f, open('standings_games.md', 'a', encoding='utf-8') as out:
-    out.writelines(['|' + '|'.join(f.readline().split()) + '|\n', '|-' * 9 + '|\n'])
-    for line in f:
-        out.write('|' + '|'.join(line.split()) + '|\n')
