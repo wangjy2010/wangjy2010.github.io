55c34bad19287ff3aebc3612f447879a335dfbdd
Wang Jingyang <jywang602@gmail.com>
2026-03-21 11:31:12 +0800

    added games standings

--- export.py
+++ export.py
@@ -1,4 +1,4 @@
-with open('bayeselo.txt', 'r', encoding='utf-8') as f, open('standings.md', 'a', encoding='utf-8') as out:
+with open('bayeselo_games.txt', 'r', encoding='utf-8') as f, open('standings_games.md', 'a', encoding='utf-8') as out:
     out.writelines(['|' + '|'.join(f.readline().split()) + '|\n', '|-' * 9 + '|\n'])
     for line in f:
         out.write('|' + '|'.join(line.split()) + '|\n')
